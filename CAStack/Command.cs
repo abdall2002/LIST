f1f05f8457c192b8f2e133a11bbefa99082e8a27
unknown <abdallahaliahmed517@gmail.com>
2023-08-14 14:24:41 +0300

    add

--- CAStack/Command.cs
+++ CAStack/Command.cs
@@ -13,6 +13,12 @@ namespace CAStack
             _createdAt = createdAt;
             _url = url;
         }
+
+        public Command(string url)
+        {
+            _createdAt = DateTime.Now;
+            _url = url;
+        }
         public override string ToString()
         {
             return $"[{this._createdAt.ToString("yyyy-MM-dd hh:mm")}] {this._url}";
--- CAStack/Program.cs
+++ CAStack/Program.cs
@@ -2,3 +2,83 @@
 
 
 /* Stack */
+
+using CAStack;
+using System.Collections.Generic;
+
+Stack <Command> undo = new Stack<Command>();
+Stack <Command> redo = new Stack<Command>();
+
+string line;
+
+while(true)
+{
+    Console.Write("Url ('exit' to quit): ");
+    line = Console.ReadLine().ToLower();
+    if (line == "exit")
+    {
+        break;
+    }
+    else if(line == "back")
+    {
+        if (undo.Count > 0)
+        {
+            var item = undo.Pop();
+            redo.Push(item);
+        }
+        else
+        {
+            continue;
+        }
+    }
+    else if(line == "forward")
+    {
+        if (redo.Count > 0)
+        {
+            var item = redo.Pop();
+            undo.Push(item);
+        }
+        else
+        {
+            continue;
+        }
+    }
+    else
+    {
+        // add url to undo list
+        undo.Push(new Command(line));
+    }
+    Console.Clear();
+
+    Print("Back", undo);
+    Print("Forward", redo);
+
+} // end of while
+
+    Stack<int> numbers = new Stack<int>(new List<int> { 1, 2, 3 });
+
+    while (numbers.Count > 0)
+    {
+        var n = numbers.Peek();
+
+        Console.WriteLine(n);
+    }
+ // end of main
+
+
+
+
+void Print(string name, Stack<Command> commands)
+{
+    Console.WriteLine($"{name} history");
+    Console.BackgroundColor = name.ToLower() == "back" ? ConsoleColor.DarkGreen : ConsoleColor.DarkBlue;
+    foreach (var u in commands)
+    {
+        Console.WriteLine($"\t{u}");
+    }
+    Console.BackgroundColor = ConsoleColor.Black;
+}
+
+
+
+
