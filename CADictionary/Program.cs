d17d871b657a8cb63620cfc69974d108ceee4ca4
unknown <abdallahaliahmed517@gmail.com>
2023-08-14 12:25:07 +0300

    gg

--- CADictionary/Program.cs
+++ CADictionary/Program.cs
@@ -2,6 +2,8 @@
 
 
 
+using CADictionary;
+
 var article =
     "Dot NET is a free cross-platform and open source developer platform" +
     "for building many different types of applications" +
@@ -33,3 +35,33 @@ foreach (var entry in lettersDictionary)
     }
 }
 
+Console.WriteLine("------------------------------------------");
+var emps = new List<Employee>
+            {
+                new Employee {Id = 100, Name = "Reem S.", ReportTo = null},
+                new Employee {Id = 101, Name = "Raed M.", ReportTo = 100 },
+                new Employee {Id = 102, Name = "Ali B.", ReportTo = 100 },
+                new Employee {Id = 103, Name = "Abeer S.", ReportTo = 102 },
+                new Employee {Id = 104, Name = "Radwan N.", ReportTo = 102 },
+                new Employee {Id = 105, Name = "Nancy R.", ReportTo = 101 },
+                new Employee {Id = 106, Name = "Saleh A.", ReportTo = 104 }
+            };
+
+//var managers = emps.GroupBy(x => x.ReportTo);
+var managers = emps.ToLookup(x => x.ReportTo).ToDictionary(x => x.Key ?? -1, x => x.ToList());
+
+foreach (var entry in managers)
+{
+    if (entry.Key == -1)
+        continue;
+    var manager = emps.FirstOrDefault(x => x.Id == entry.Key);
+
+    Console.WriteLine($"{manager}");
+
+    foreach (var emp in entry.Value)
+    {
+        Console.WriteLine($"\t\t\"{emp}\"");
+    }
+}
+
+Console.ReadKey();
