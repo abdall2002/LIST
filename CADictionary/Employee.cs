0b44ca2663df5b5294b7da77e27c3663b15edc22
unknown <abdallahaliahmed517@gmail.com>
2023-08-14 12:25:47 +0300

    add Employee

---
+++ CADictionary/Employee.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+namespace CADictionary
+{
+    public class Employee
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public int? ReportTo { get; set; }
+
+        public override string ToString()
+        {
+            return $"[{Id}] {Name}";
+        }
+    }
+
+}
