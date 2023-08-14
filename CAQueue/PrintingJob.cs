72c6f7a643e9a58b9ea0a828d1070a47e1d9e37c
unknown <abdallahaliahmed517@gmail.com>
2023-08-14 15:24:03 +0300

    add

---
+++ CAQueue/PrintingJob.cs
@@ -0,0 +1,23 @@
+﻿using System;
+
+namespace CAQueue
+{
+	public class PrintingJob
+    {
+		private readonly string _file;
+		private readonly int _copies;
+
+        public PrintingJob(string file, int copies)
+        {
+            _file = file;
+            _copies = copies;
+        }
+
+        public override string ToString()
+        {
+            return $"{_file} x #{_copies} copies";
+        }
+    }
+
+	
+}
