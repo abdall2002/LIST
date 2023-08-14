62fd8c1abd04404cfc7b36a2da9632b1aa054ebe
unknown <abdallahaliahmed517@gmail.com>
2023-08-14 13:29:32 +0300

    add

---
+++ CAStack/Command.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+
+namespace CAStack
+{
+	public class Command
+	{
+        private readonly DateTime _createdAt;
+        private readonly string _url;
+
+        public Command(DateTime createdAt, string url)
+        {
+            _createdAt = createdAt;
+            _url = url;
+        }
+        public override string ToString()
+        {
+            return $"[{this._createdAt.ToString("yyyy-MM-dd hh:mm")}] {this._url}";
+        }
+    }
+}
