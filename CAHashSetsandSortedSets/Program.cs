f6d1c435ac135d833c0bc42253e7f1819547a4cb
unknown <abdallahaliahmed517@gmail.com>
2023-08-15 12:51:51 +0300

    add

--- CAHashSetsandSortedSets/Program.cs
+++ CAHashSetsandSortedSets/Program.cs
@@ -1,13 +1,18 @@
 ﻿// See https://aka.ms/new-console-template for more information
 
+using CAHashSetsandSortedSets;
+
+var cust1 = new Customer { Name = "Essam A", Telephone = "+1 123 123 4533" };
+var cust2 = new Customer { Name = "Essam A", Telephone = "+1 123 123 4533" };
+Console.WriteLine(cust1.Name.GetHashCode());             // -> print -346360240
+Console.WriteLine(cust1.Equals(cust2));                  // -> True
+Console.WriteLine(object.ReferenceEquals(cust1,cust2));  // -> False
 
 
 
 
 
-
-
-
+Console.ReadKey();
 
 
 
