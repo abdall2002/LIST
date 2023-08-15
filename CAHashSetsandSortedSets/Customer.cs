5339a6a4e7b0547110304737f46430602687548c
unknown <abdallahaliahmed517@gmail.com>
2023-08-15 12:41:38 +0300

    add

---
+++ CAHashSetsandSortedSets/Customer.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+namespace CAHashSetsandSortedSets
+{
+
+    public class Customer
+    {
+        public string Name { get; set; }
+        public string Telephone { get; set; }
+
+        public override int GetHashCode()
+        {
+            var hash = 17;
+            hash = hash * 397 + Name.GetHashCode();
+            return hash;
+        }
+        public override string ToString()
+        {
+            return $"{Name} ({Telephone})";
+        }
+    }
+}
