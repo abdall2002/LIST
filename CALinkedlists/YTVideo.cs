9b6535740c67ba8607ffab5af2721128ab41d8a0
unknown <abdallahaliahmed517@gmail.com>
2023-08-15 11:53:07 +0300

    add

--- CALinkedlists/YTVideo.cs
+++ CALinkedlists/YTVideo.cs
@@ -2,8 +2,18 @@
 
 namespace CALinkedlists
 {
-	public class YTVideo
-	{
-		
-	}
+    public class YTVideo
+    {
+        public string Id { get; set; }
+        public string Title { get; set; }
+        public TimeSpan Duration { get; set; }
+
+        public override string ToString()
+        {
+            // C# Variables (00:30:00)
+            //      https://www.youtube.com/watch?v=d6EpL33g9tg
+            return $"├── {Title} ({Duration})\n│\thttps://www.youtube.com/watch?v={Id}";
+        }
+    }
+    
 }
