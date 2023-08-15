f0016d9d47bd3388b713fa6278f4b65acacbdda0
unknown <abdallahaliahmed517@gmail.com>
2023-08-15 12:08:26 +0300

    add

--- CALinkedlists/Program.cs
+++ CALinkedlists/Program.cs
@@ -1,2 +1,38 @@
 ﻿// See https://aka.ms/new-console-template for more information
 
+using CALinkedlists;
+
+var lesson1 = new YTVideo { Id = "YTV1", Title = "Variables", Duration = new TimeSpan(00, 30, 00) };
+var lesson2 = new YTVideo { Id = "YTV2", Title = "Classes and Strcts", Duration = new TimeSpan(01, 20, 00) };
+var lesson3 = new YTVideo { Id = "YTV3", Title = "Expressions", Duration = new TimeSpan(00, 45, 00) };
+var lesson4 = new YTVideo { Id = "YTV4", Title = "Iterations", Duration = new TimeSpan(01, 10, 00) };
+var lesson5 = new YTVideo { Id = "YTV5", Title = "Generics", Duration = new TimeSpan(00, 20, 00) };
+
+
+LinkedList<YTVideo> playlist = new LinkedList<YTVideo>(new YTVideo[] {
+    lesson1,
+    lesson2,
+    lesson3,
+    lesson4,
+    lesson5
+});
+Print("C# from zero to hero", playlist);
+Console.ReadKey();
+void Print(string title, LinkedList<YTVideo> playlist)
+        {
+    Console.WriteLine($"┌{title}");
+    foreach (var item in playlist)
+        Console.WriteLine(item);
+    Console.WriteLine($"└");
+    Console.WriteLine($"Total: {playlist.Count} item(s)");
+
+}
+
+
+
+
+
+
+
+
+
